8c41728b96b0233d7db63acf883b41ef00e235bf
yankang.liu <Cooper_liu@outlook.com>
2020-07-08 13:14:46 +0800

    update run.bat script

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-dotnet build .\Dotnet.Performance.Labs\Dotnet.Performance.Labs.csproj -c Release & dotnet run .\Dotnet.Performance.Labs\Dotnet.Performance.Labs.csproj -c Release artifacts=./result
+dotnet build .\Dotnet.Performance.Labs\Dotnet.Performance.Labs.csproj -c Release & dotnet run -p .\Dotnet.Performance.Labs\Dotnet.Performance.Labs.csproj -c Release
